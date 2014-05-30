cdf1dc9d8516b9b25ae8ca63034b277335a91e1c
kandelsoftware <kandelsoftware@gmail.com>
2014-05-30 23:14:34 +0200

    change in environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,20 +51,18 @@ Restaurant::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-#config.action_mailer.default_url_options = { :host => 'http://188.226.212.220' }
+config.action_mailer.default_url_options = { :host => '188.226.212.220' }
   # Show full error reports and disable caching
-   config.action_mailer.default_url_options = { :host => '' }
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
   config.action_mailer.delivery_method=:smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => 'localhost',
-    :user_name => "himalayarestaurante@gmail.com",
-    :password => "@badalona",
+    :domain => '188.226.212.220',
+    :user_name => "reservahimalaya@gmail.com",
+    :password => "ramkandel33",
     :authentication => 'plain',
     :enable_starttls_auto => true
+
   }
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
