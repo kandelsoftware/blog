0c50bfe3092c7e9f0962c63aff9a3aa95fb9e613
kandelsoftware <kandelsoftware@gmail.com>
2014-06-06 16:03:07 +0200

    change in css

--- app/models/himalaya.rb
+++ app/models/himalaya.rb
@@ -7,11 +7,11 @@ class Himalaya < ActiveRecord::Base
   validates_presence_of :menu, :on => :create,       :message => "Menu no puede estar vacio"
   validates_presence_of :fecha, :on => :create,       :message => "Fecha no puede estar vacio"
   validates_presence_of :hora, :on => :create,        :message => "Hora no puede estar vacio"
-  validates_presence_of :nombre, :on => :create,       :message =>"Nombre no puede estar vacio"
-  validates_presence_of :apellidos, :on => :create,       :message =>"Apellidos no puede estar vacio"
+  validates_presence_of :nombre, :on => :create,       :message =>" introducir  nombre. "
+  validates_presence_of :apellidos, :on => :create,       :message =>"introducir  apellido"
    validates :telefono,   :presence => {:message => 'entrar numero corecto!'},
                      :numericality => true,
-                     :length => { :minimum => 10, :maximum => 15 }
+                     :length => { :minimum => 9, :maximum => 15 }
   validates_presence_of :telefono, :on => :create,     :message =>"Telefono no  puede estar vacio"
 end
 
